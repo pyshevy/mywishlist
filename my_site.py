909a00523cb0886bc3d91e047b732346cbba0d48
pyshevy <141495387+pyshevy@users.noreply.github.com>
2024-06-24 22:06:13 +0500

    Update my_site.py

--- my_site.py
+++ my_site.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import os
+import sys
 from threading import Thread
 
 from loader import base
